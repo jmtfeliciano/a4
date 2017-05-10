f13622719e1a13e65ec8e071dbcc6cf935fadb6f
Josemari Feliciano <josemari.feliciano@gmail.com>
2017-05-10 16:22:25 -0400

    Updated migrations's up method to accomodate database edits

--- database/migrations/2017_05_10_195436_create_planets_table.php
+++ database/migrations/2017_05_10_195436_create_planets_table.php
@@ -13,7 +13,23 @@ class CreatePlanetsTable extends Migration
      */
     public function up()
     {
-        //
+
+     	Schema::create('planets', function (Blueprint $table) {
+
+     		# Increments method will make a Primary, Auto-Incrementing field.
+     		# Most tables start off this way
+     		$table->increments('id');
+
+     		# This generates two columns: `created_at` and `updated_at` to
+     		# keep track of changes to a row
+     		$table->timestamps();
+
+     		# The rest of the fields...
+     		$table->string('name');
+
+     		# FYI: We're skipping the 'tags' field for now; more on that later.
+
+     	});
     }
 
     /**
