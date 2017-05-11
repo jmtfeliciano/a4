1894940b5a6a28ba24c790f517c89b12543a37d2
Josemari Feliciano <josemari.feliciano@gmail.com>
2017-05-11 14:14:50 -0400

    Added a planetary folder to handle all the subsequent view pages

---
+++ resources/planetarypages/navi.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.master')
+
+@section('purpose')
+    Open-source Planetary Database
+@endsection
+
+@section('content')
+     <h3>Welcome to our open-source planetary database!</h3><br><br>
+
+     <h4>Indicate which you want to do below! </h4> <br><br>
+
+@endsection
